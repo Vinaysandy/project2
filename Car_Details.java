ef2f46f7fce72cec4e3439dca29fa96ecd36b6c6
Your Name <vinayraj.up96@gmail.com>
2022-12-14 18:22:05 +0530

    2nd Code

---
+++ Car_Details.java
@@ -0,0 +1,16 @@
+class Car_Details
+  {
+    public static void main (String [] args)
+      {
+        String name="BBT";
+	int CC=6600;
+	long Price=18933821l;
+	String colour="Gray";
+	String Fuel_type="Petrol";
+	System.out.println ("The Car Name is "+ name);
+	System.out.println ("The CC is "+ CC);
+        System.out.println ("The Price Is "+ Price);
+	System.out.println ("The colour is "+ colour);
+	System.out.println ("The Fuel type is "+ Fuel_type);
+      }
+}
